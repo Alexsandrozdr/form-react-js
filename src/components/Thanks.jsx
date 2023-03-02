d2600c1480966001b379304c4d1042c56916b153
alexsandrozdr <andynhozdr@gmail.com>
2023-03-02 03:04:05 -0300

    formulário com validação

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -24,7 +24,9 @@ const Thanks = ({ data }) => {
         em descontos para a sua próxima compra.
       </p>
       <p>Para concluir sua avaliação clique no botão abaixo.</p>
-      <h3>Aqui está o resumo da sua avaliação {data.name}:</h3>
+      <h3>
+        Muito obrigado {data.name}! <br /> Aqui está o resumo da sua avaliação:
+      </h3>
       <p className="review-data">
         <span>Satisfação com o produto:</span>
         {emojiData[data.review]}
